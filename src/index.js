bdad02a53bd8ac037ae277b8086b1eb6f5abcac7
Nguyen Duy Cuong <cuongw.me@gmail.com>
2019-08-27 22:52:37 +0700

    Setup basic prisma

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-const { prisma } = require('./generated/prisma-client');
+const { prisma } = require('../generated/prisma-client');
 
 // A `main` function so that we can use async/await
 async function main() {
-  // Create a new user called `Alice`
-  const newUser = await prisma.createUser({ name: 'Alice' });
-  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`);
-
-  // Read all users from the database and print them to the console
-  const allUsers = await prisma.users();
-  console.log(allUsers);
+  // Read the previously created user from the database and print their posts to the console
+  const postsByUser = await prisma.user({ email: 'cuongw.me@gmail.com' }).posts()
+  console.log(`All posts by that user: ${JSON.stringify(postsByUser)}`)
 }
 
 main().catch(e => console.error(e));
