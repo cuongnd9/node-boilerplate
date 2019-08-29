a67596b299726a4bf9544eba30d62379a1f68600
Cuong Duy Nguyen <cuongw.me@gmail.com>
2019-08-29 22:56:13 +0700

    Setup absolute imports (#27)

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import userController from '../controllers/user.controller';
+import userController from '@/controllers/user.controller';
 
 const router = express.Router();
 
