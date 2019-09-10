6be4034faf9f0bf549e7a25a9a3d553deac4767f
Cuong Duy Nguyen <cuongw.me@gmail.com>
2019-09-10 23:52:52 +0700

    Update error handler (#75)

--- src/helpers/errorHandler.js
+++ src/helpers/errorHandler.js
@@ -1,7 +1,11 @@
 import Boom from '@hapi/boom';
 
 export default function handleError(err, req, res, next) {
-  if (typeof err !== 'object' || (!err.statusCode && !err.output.statusCode)) {
+  if (
+    typeof err !== 'object' ||
+    Object.keys(err).length === 0 ||
+    (!err.statusCode && !err.output.statusCode)
+  ) {
     const defaultError = Boom.badImplementation('An internal server error occurred');
     const { statusCode, payload } = defaultError.output;
     return res.status(statusCode).json(payload);
